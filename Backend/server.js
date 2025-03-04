b00bfaf9e750c7bf53540192210ba0dbcbd7acc9
farhamkh <fk61462n@pace.edu>
2025-03-04 11:02:20 -0500

    Connected backend with Mongodb

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config(); 
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
