29e90038fdc1100818ed39fe0c6259a7b7d8468e
farhamkh <fk61462n@pace.edu>
2025-03-04 11:07:50 -0500

    Removed Deprecated Options

--- Backend/server.js
+++ Backend/server.js
@@ -12,8 +12,6 @@ app.use(cors());
 
 // MongoDB 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("connection error:", err));
 
