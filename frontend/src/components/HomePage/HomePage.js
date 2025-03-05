800fc706642b233b7bed83ae35118c135dda0f08
sharondsouza <sharondsouza26@gmail.com>
2025-03-05 04:21:16 -0500

    Start up screen

---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,41 @@
+import { Container, Button } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
+
+import './HomePage.css'
+
+function HomePage() {
+    const navigate = useNavigate();
+
+    const handleContinue = () => {
+        navigate("/theme-selection");
+      };
+
+    return (
+        <div className="homepage">
+        {/* Background Image */}
+        <div className="homepage-background">
+          <Container className="homepage-content">
+            {/* Header */}
+            <h1 className="homepage-header">Q U E S T</h1>
+            {/* Paragraph */}
+            <p className="homepage-paragraph">
+            <i>A journey into the unknown, the choice is up to you</i>
+            </p>
+            <p>
+            click "Continue" to begin
+            </p>
+            {/* Continue Button */}
+            <Button
+              className="continue-button"
+              variant="primary"
+              onClick={handleContinue}
+            >
+              Continue
+            </Button>
+          </Container>
+        </div>
+      </div>
+    )
+}
+
+export default HomePage
