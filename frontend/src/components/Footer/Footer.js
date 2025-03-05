95e2c8653e3017753c4a6317a8a051cd98d22b34
sharondsouza <sharondsouza26@gmail.com>
2025-03-05 04:20:27 -0500

    Footer component

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import { Container, Row, Nav } from 'react-bootstrap'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <Container className='Footer' fluid>
+                <i>QuestAI | Designed and Built by Codesaurus</i>
+
+            </Container>
+        </footer >
+    )
+}
+
+export default Footer
