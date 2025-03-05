2d2f4e977dc11a5051fa6fbedd65837030035e01
sharondsouza <sharondsouza26@gmail.com>
2025-03-05 04:22:37 -0500

    static UI screens with API connection pending

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer/Footer';
+import HomePage from "./components/HomePage/HomePage";
+import ThemePage from "./components/ThemePage/ThemePage";
+import StoryPage from "./components/StoryPage/StoryPage";
+
+
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-wrapper">
+
+        {/* Home Screen */}
+        <Container className="home-screen" fluid>
+          <Routes>
+            Default Route
+
+            <Route path="/" element={ <HomePage /> }/>
+            
+            <Route path="/theme-selection" element={<ThemePage />} />
+            <Route path="/storie-selection" element={<StoryPage />} />
+
+          </Routes>
+        </Container>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
