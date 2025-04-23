efd1390210179589433d336d8405b60d39624038
farhamkh <fk61462n@pace.edu>
2025-04-23 11:21:19 -0400

    Set up multer

---
+++ Backend/routes/uploadRoutes.js
@@ -0,0 +1,24 @@
+// routes/uploadRoutes.js
+const express = require("express");
+const router = express.Router();
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const s3 = require("../s3"); 
+
+// Set up multer to upload to S3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      const fileName = `npc-images/${Date.now().toString()}-${file.originalname}`;
+      cb(null, fileName);
+    },
+  }),
+});
+
